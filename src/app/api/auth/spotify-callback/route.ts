417f386dac6f041be1393da580fa01ea51c65cf2
Elena Gonzalez <egonzalezcm08@gmail.com>
2023-11-18 11:20:38 +0100

    Cookies expire (every 24h)

--- src/app/api/auth/spotify-callback/route.ts
+++ src/app/api/auth/spotify-callback/route.ts
@@ -5,19 +5,22 @@ import { redirect } from "next/navigation";
 
 
 export async function GET(req: NextRequest) {
+  
+  const oneDay = 24 * 60 * 60 * 1000
+
   const params = req.nextUrl.searchParams;
   const code = params.get('code');
   if (!code) return Response.json({message: 'Error retrieving code in Spotify Callback URL'});
 
   const state = params.get('state');
-  if (state) cookies().set('state', state);
+  if (state) cookies().set('state', state, {expires: new Date(Date.now() + oneDay)});
 
 
   const tokenResponse = await getAuthToken(code);
   if (tokenResponse) {
     const { access_token, refresh_token } = tokenResponse;
-    cookies().set('spotify_access_token', access_token);
-    cookies().set('spotify_refresh_token', refresh_token);
+    cookies().set('spotify_access_token', access_token, {expires: new Date(Date.now() + oneDay)});
+    cookies().set('spotify_refresh_token', refresh_token, {expires: new Date(Date.now() + oneDay)});
   }
   redirect('/');
 }
