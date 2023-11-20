0952038c74a41fd48173bd1f21b14c08ae6fedaa
wangjianing <jianingmica@gmail.com>
2023-11-20 14:29:51 +0100

    style: format w/ prettier

--- src/lib/utils/spotifyUtils.ts
+++ src/lib/utils/spotifyUtils.ts
@@ -1,40 +1,56 @@
 import { Release, Track } from '@prisma/client';
 import { shuffleArray } from './utils';
 
-import { fetchTracksDetails, fetchTopItems, fetchArtistTopTracks,fetchAlbum, fetchAlbums, searchAlbum } from '@/lib/services/spotifyServices'
-import { parseSpotifyAlbumToRelease } from '@/lib/utils/releaseUtils'
+import {
+  fetchTracksDetails,
+  fetchTopItems,
+  fetchArtistTopTracks,
+  fetchAlbum,
+  fetchAlbums,
+  searchAlbum,
+} from '@/lib/services/spotifyServices';
+import { parseSpotifyAlbumToRelease } from '@/lib/utils/releaseUtils';
 
-
-export async function getTracksDetails(trackIds: string[], accessToken: string): Promise<any[] | void> {
+export async function getTracksDetails(
+  trackIds: string[],
+  accessToken: string
+): Promise<any[] | void> {
   try {
-    const tracks = await fetchTracksDetails(trackIds,accessToken)
+    const tracks = await fetchTracksDetails(trackIds, accessToken);
 
     return tracks;
-
   } catch (error) {
     console.log('ERROR - getTracksDetails', error);
   }
 }
 
-export async function getSeveralArtistsTracks(artistsIds: string[], accessToken: string) {
+export async function getSeveralArtistsTracks(
+  artistsIds: string[],
+  accessToken: string
+) {
   try {
-    const artistsTracks = await Promise.all(artistsIds.map(async (id) => {
-      const topTracks = await fetchArtistTopTracks(id, accessToken);
-      return topTracks;
-    }));
+    const artistsTracks = await Promise.all(
+      artistsIds.map(async id => {
+        const topTracks = await fetchArtistTopTracks(id, accessToken);
+        return topTracks;
+      })
+    );
 
     return artistsTracks;
-  } catch (error) {
-  }
+  } catch (error) {}
 }
 
-export async function getTopItems(type: string, accessToken: string, limit: number) {
+export async function getTopItems(
+  type: string,
+  accessToken: string,
+  limit: number
+) {
   try {
     const items = await fetchTopItems(type, accessToken, limit);
     // console.log(type, ' - items :>> ', items[0]);
 
     if (items) return items;
-    else throw new Error("empty items");
+    else throw new Error('empty items');
   } catch (error) {
     console.log('ERROR - getTopItems', error);
   }
@@ -42,14 +58,13 @@ export async function getTopItems(type: string, accessToken: string, limit: numb
 
 export function extractItemIds(items: any[]): string[] | void {
   try {
-
-    if (!items) throw new Error("extractItemIds - No items");
+    if (!items) throw new Error('extractItemIds - No items');
     // console.log('extractItemIds - items :>> ', items[0]);
     const itemIds = [];
     for (let item of items) itemIds.push(item.id);
 
     if (itemIds) return itemIds;
-    else throw new Error("empty items");
+    else throw new Error('empty items');
   } catch (error) {
     console.log('ERROR - getTopItems', error);
   }
@@ -60,7 +75,7 @@ export async function getAlbums(ids: string[], accessToken: string) {
     if (!ids) return;
     shuffleArray(ids);
 
-    const albums = await fetchAlbums(ids,accessToken)
+    const albums = await fetchAlbums(ids, accessToken);
 
     return albums;
   } catch (error) {
@@ -69,20 +84,25 @@ export async function getAlbums(ids: string[], accessToken: string) {
 }
 
 // not used
-export async function getSeveralTracks(trackIds:string[], accessToken:string) {
-
-  const data = await fetchTracksDetails(trackIds, accessToken)
+export async function getSeveralTracks(
+  trackIds: string[],
+  accessToken: string
+) {
+  const data = await fetchTracksDetails(trackIds, accessToken);
 
   return data;
 }
 
 // TODO: solve why this URL is not correctly fetching data - error 400
-export async function searchSpotifyAlbum(album:string, artist:string, accessToken:string):Promise<Release> {
-  const res = await searchAlbum(album, artist, accessToken)
+export async function searchSpotifyAlbum(
+  album: string,
+  artist: string,
+  accessToken: string
+): Promise<Release> {
+  const res = await searchAlbum(album, artist, accessToken);
 
-  const topAlbum = res.albums.items[0]
-  const topAlbumDetail = await fetchAlbum(topAlbum.id, accessToken)
+  const topAlbum = res.albums.items[0];
+  const topAlbumDetail = await fetchAlbum(topAlbum.id, accessToken);
 
   return parseSpotifyAlbumToRelease(topAlbumDetail);
-
 }
