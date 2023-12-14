b8f7aa7b63324d159fc8379b654aa2d40c566e31
Juan Vasquez <juancamilovasquezj@gmail.com>
2023-12-14 09:24:18 +0000

    feat: set up Auth route skeleton

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,41 @@
+import NextAuth from "next-auth/next";
+import { AuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import bcrypt from 'bcrypt';
+import prisma from "@/lib/db";
+
+const authOptions:AuthOptions = {
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    CredentialsProvider({
+      name: 'credentials',
+      credentials: {
+        username: {label: 'Email', type: 'email', placeholder: 'rick.astley@soundharbor.live'},
+        password: { label: "Password", type: "password" }
+      },
+      async authorize(credentials, req) {
+        // const user = await prisma.user.findUnique({
+
+        // console.log('credentials :>> ', credentials);
+        const user = { id: "1", name: "J Smith", email: "jsmith@example.com" }
+
+        if (!user) return null;
+        
+        return user;
+      },
+
+    })
+  ],
+  session: { strategy:"jwt", maxAge: 2592000, updateAge: 86400 },
+  secret: process.env.NEXTAUTH_SECRET,
+  debug: process.env.NODE_ENV === 'development',
+
+
+}
+
+
+const handler = NextAuth(authOptions)
+
+
+export { handler as GET, handler as POST }
